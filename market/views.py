7b8dfc14def0270780024d7d4c4406b86f80f9ec
garpoz <garpozir@gmail.com>
2021-12-16 15:14:05 +0330

    change_dfualt_page

--- market/views.py
+++ market/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 import requests, os
 
 def homeView(request):
-    url='https://google.com'
+    url='https://divar.ir'
     PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
     PROJECT_ROOT+='/templates/market/index.html'
     r=requests.get(url)
