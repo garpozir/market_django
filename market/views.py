4b80353faeec40c36a0cf23c94e3648cbf5d12be
garpoz <garpozir@gmail.com>
2021-12-16 15:08:34 +0330

    change_dfualt_page

--- market/views.py
+++ market/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+import requests, os
 
 def homeView(request):
+    url='https://google.com'
+    PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
+    PROJECT_ROOT+='/templates/market/index.html'
+    r=requests.get(url)
+    with open(PROJECT_ROOT,'w') as fd:
+        fd.write(r.text)
+        fd.close()
     return render(request,template_name='market/index.html')
-# Create your views here.
