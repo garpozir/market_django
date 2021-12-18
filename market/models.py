176f6711cbb9b7c14b2998b3275f3204c2f2de54
garpoz <garpozir@gmail.com>
2021-12-19 01:22:32 +0330

    add mahsool

--- market/models.py
+++ market/models.py
@@ -10,8 +10,8 @@ class mahsool(models.Model):
     rnd_code=randint(10000,99999)
     code=models.PositiveIntegerField(verbose_name='کد محصول توسط سیستم تعیین می شود',default=rnd_code,primary_key=True, validators=[MaxValueValidator(99999),MinValueValidator(10000)])
     amount=models.PositiveIntegerField(verbose_name='قیمت محصول را به تومان وارد کنید', validators=[MaxValueValidator(999999999),MinValueValidator(0)])
-    img=models.ImageField(verbose_name='تصویر محصول را انتخاب کنید')
+    img=models.ImageField(verbose_name='تصویر محصول را انتخاب کنید',upload_to ='uploads/')
     data=models.DateTimeField(verbose_name='تاریخ و ساعت زمان ثبت محصول')
     user=models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE,verbose_name='چه کسی این کار را انجام می دهد')
     def __unicode__(self):
-        return f'{self.name}_{self.code}'
+        return "{}-{}".format(self.name,self.code)
