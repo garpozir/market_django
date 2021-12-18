dc4ef3daf4a7854cde38557483a262622c930983
garpoz <garpozir@gmail.com>
2021-12-19 00:09:43 +0330

    add mahsool

--- market/models.py
+++ market/models.py
@@ -1,3 +1,17 @@
+from __future__ import unicode_literals
+from django.core.validators import MaxValueValidator
+from django.conf import settings
 from django.db import models
+from random import randint
 
-# Create your models here.
+class mahsool(models.Model):
+    name=models.CharField(max_length=100,verbose_name='نام محصول را وارد کنید')
+    info=models.TextField(verbose_name='توضیحات محصول را اینجا بنویسید',max_length=2000)
+    rnd_code=randint(10000,99999)
+    code=models.PositiveIntegerField(verbose_name='کد محصول توسط سیستم تعیین می شود',default=rnd_code,primary_key=True, validators=[MaxValueValidator(99999)])
+    amount=models.PositiveIntegerField(verbose_name='قیمت محصول را به تومان وارد کنید', validators=[MaxValueValidator(999999999)])
+    img=models.ImageField(verbose_name='تصویر محصول را انتخاب کنید')
+    data=models.DateTimeField(verbose_name='تاریخ و ساعت زمان ثبت محصول')
+    user=models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE,verbose_name='چه کسی این کار را انجام می دهد')
+    def __unicode__(self):
+        return f'{self.name}_{self.code}'
--- market/views.py
+++ market/views.py
@@ -1,12 +1,4 @@
 from django.shortcuts import render
-import requests, os
 
 def homeView(request):
-    url='https://divar.ir'
-    PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
-    PROJECT_ROOT+='/templates/market/index.html'
-    r=requests.get(url)
-    with open(PROJECT_ROOT,'w') as fd:
-        fd.write(r.text)
-        fd.close()
     return render(request,template_name='market/index.html')
