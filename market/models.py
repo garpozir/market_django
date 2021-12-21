f534ee98f20ecf67341462af5aee3f5e03f76792
Behrouz_Ashraf <garpozir@gmail.com>
2021-12-21 00:02:08 -0500

    edit models

--- market/models.py
+++ market/models.py
@@ -12,6 +12,8 @@ class mahsool(models.Model):
     rnd_code=randint(10000,99999)
     code=models.PositiveIntegerField(verbose_name='کد محصول توسط سیستم تعیین می شود',default=str(rnd_code),primary_key=True, validators=[MaxValueValidator(99999),MinValueValidator(10000)])
     amount=models.PositiveIntegerField(verbose_name='قیمت محصول را به تومان وارد کنید', validators=[MaxValueValidator(999999999),MinValueValidator(0)])
+    number=models.PositiveIntegerField(verbose_name='تعداد محصول', validators=[MaxValueValidator(999),MinValueValidator(0)])
+    ofer=models.PositiveIntegerField(verbose_name='درصد تخفیف',default=str(0), validators=[MaxValueValidator(99),MinValueValidator(0)])
     img=models.ImageField(verbose_name='تصویر محصول را انتخاب کنید',upload_to ='uploads/')
     data=models.DateTimeField(verbose_name='تاریخ و ساعت زمان ثبت محصول')
     user=models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE,verbose_name='چه کسی این کار را انجام می دهد')
