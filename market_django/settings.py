2ad5e75a0a09ccdd2094e0cf2a4ac70bfa3d2205
Behrouz_Ashraf <garpozir@gmail.com>
2021-12-22 02:45:09 -0500

    edit index.html

--- market_django/settings.py
+++ market_django/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import os.path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -124,7 +125,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-
+SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
